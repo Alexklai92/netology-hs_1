b261ad894c0252e4fd3de4d0e4e3d6908fb3eeea
Alexklai92 <aklai@inbox.ru>
2019-05-26 11:26:31 +0300

    1

---
+++ errors/js/ui.js
@@ -0,0 +1,6 @@
+'use strict';
+document.getElementById('open').onclick = function handleClick() {
+  openMail();
+}
+// const button = document.getElementById('open');
+// button.onclick = handleClick();
